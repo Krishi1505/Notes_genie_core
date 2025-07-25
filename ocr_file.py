21196908fe46cad7dec185641b3bbc1cf04d95df
Krishi1505 <krishighumare@gmail.com>
2025-07-25 22:13:04 +0530

    deleted certain files

--- ocr_file.py
+++
@@ -1,7 +0,0 @@
-import easyocr
-
-def extract_text(image_path):
-    reader = easyocr.Reader(['en'])
-    results = reader.readtext(image_path)
-    text = " ".join([res[1] for res in results])
-    return text
